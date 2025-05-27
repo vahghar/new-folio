6fbcc1c409e7c97ffd08e5b14621a556749fc835
vahghar <raghav10g@gmail.com>
2025-05-27 09:39:05 +0530

    fiexd-project_id

--- src/components/VedWrapper.tsx
+++ src/components/VedWrapper.tsx
@@ -5,7 +5,7 @@ import { VedAnalytics } from 'ved-analytics';
 const VedWrapper: React.FC = () => {
   return (
     <VedAnalytics
-      project='portfolio'
+      project='709432f5-882e-44bf-b989-798fea86ac20'
       apiKey='OmasPbBbzMhbjrmFwxXlDdh2oDXt8jZJ8eaRjnhUwmwnOHO+esaOgV17D4DKl80+pYaCk34nNU2wDiWrwD2jNwT2HDjA8SWCrfYxNbxnPoXkPh2Ru7kSQsBilHImCN19C9diwSsmpZfkTDgLYtE3Datun8p2sg4T0H0WlDiYiZQ='
     />
   );
